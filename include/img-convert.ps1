e45f28db99caf8ca0c82756c8c4cd0ed7a1b2a1c
pwindle <pwindle11@gmail.com>
2020-04-04 16:30:09 +0100

    Generalise image conversion

---
+++ include/img-convert.ps1
@@ -0,0 +1,31 @@
+$filetype = Read-Host "Enter the desired output file type"
+
+# Ask for quality if applicable
+function GetDesiredQuality()
+{
+	$quality = Read-Host -Prompt 'Give quality setting (1-100, default 85) '
+	if ($quality -eq "") { $quality = 85 }
+	return $quality
+}
+
+# Set command string
+switch ($filetype)
+{
+	"jpg"   { $quality = GetDesiredQuality; $command = "-quality $quality -interlace Plane -colorspace sRGB -define jpeg:dct-method=float -sampling-factor 4:2:0"; break }
+	"webp"  { $quality = GetDesiredQuality; $command = "-quality $quality"; break }
+	"png"   { $command = "-interlace Plane -colorspace sRGB"; break }
+	
+	# settings from comment thread on this stackoverflow answer: https://stackoverflow.com/a/7262050
+	# and as recommended by google for optimisation: https://developers.google.com/speed/docs/insights/OptimizeImages
+	# -colorspace sRGB to preserve colours. Seems to have no effect on file size, so no problem?
+}
+
+$count = 0
+$percent = 0
+foreach ($arg in $args) {
+	Write-Progress -Activity "Converting to $filetype" -Status "$percent_formatted% Complete:" -PercentComplete $percent;
+	Invoke-Expression "magick mogrify $command -format $filetype '$arg'"
+	$count++
+	$percent = $count / $args.count * 100
+	$percent_formatted = "{0:n1}" -f $percent
+}
